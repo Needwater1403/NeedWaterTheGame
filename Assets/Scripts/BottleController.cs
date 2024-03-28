bd8d50b67d61369e55bd1439ecc737207ec52566
KinoZero <nminhquan143@gmail.com>
2024-03-29 06:47:59 +0700

    *add gameloop

--- Assets/Scripts/BottleController.cs
+++ Assets/Scripts/BottleController.cs
@@ -5,24 +5,113 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = System.Random;
+using DG.Tweening;
+
 //using Input = InputWrapper.Input;
 public class BottleController : MonoBehaviour
 {
-    public Color[] liquidColor;
+    private enum GameState  {_start,_isPlaying, _end};
     private float _beerHight;
     private float _foamHight;
     private float _temp;
     private bool _isStop;
-    private bool _isEnd;
+    private GameState _state;
     public SpriteRenderer[] liquidSpriteRenderer;
     public SpriteRenderer sRenderer;
     [FormerlySerializedAs("Max")] public Transform max;
     [FormerlySerializedAs("Min")] public Transform min;
     public TextMeshProUGUI status;
     private static float _spriteSize;
+    private int _score = 0;
     private void Start()
     {
-        _isEnd = false;
+        ResetParam();
+    }
+
+    private void FixedUpdate()
+    {
+        Test();
+    }
+    
+
+    private void Test()
+    {
+        if (Input.touchCount > 0 && _state == GameState._start)
+        {
+            _isStop = true;
+            var touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Stationary:
+                {
+                    // GAMEPLAY
+                    if(_beerHight < 2.58f && _foamHight < 2.58f)
+                    {
+                        _beerHight += 0.015f;
+                        _foamHight += 0.025f;
+                        liquidSpriteRenderer[0].material.SetFloat("_FillAmount1", _beerHight);
+                        liquidSpriteRenderer[1].material.SetFloat("_FillAmount1", _foamHight);
+                    }
+
+                    break;
+                }
+                case TouchPhase.Ended or TouchPhase.Canceled:
+                {
+                    _isStop = false;
+                    _state = GameState._isPlaying;
+                    var c = _foamHight - _beerHight;
+                    _temp = _beerHight + c / 2.5f;
+                    break;
+                }
+            }
+        }
+        else if(_isStop)
+        {
+            _isStop = false;
+            _state = GameState._isPlaying;
+            var c = _foamHight - _beerHight;
+            _temp = _beerHight + c / 2.5f;
+        }
+        
+        if (_state == GameState._isPlaying)
+        {
+            _foamHight -= 0.015f;
+            liquidSpriteRenderer[1].material.SetFloat("_FillAmount1", _foamHight);
+            if (!(_beerHight < _temp))
+            {
+                _state = GameState._end;
+                CheckResult();
+                return;
+            }
+            _beerHight += 0.01f;
+            liquidSpriteRenderer[0].material.SetFloat("_FillAmount1", _beerHight);
+        }
+    }
+    IEnumerator SlideOutTransition()
+    {
+        min.GetComponent<SpriteRenderer>().material.DOFade(0,.6f);
+        max.GetComponent<SpriteRenderer>().material.DOFade(0,.6f);
+        yield return new WaitForSeconds(.6f);
+        var position = transform.position;
+        gameObject.transform.DOMove(new Vector3(8.8f, position.y, 0),2,false);
+        yield return new WaitForSeconds(2f);
+        gameObject.transform.position = new Vector3(-8.8f, position.y, 0);
+        ResetParam();
+        yield return null;
+    }
+
+    IEnumerator SlideInTransition()
+    {
+        gameObject.transform.DOMove(new Vector3(0, transform.position.y, 0),2,false);
+        yield return new WaitForSeconds(2f);
+        min.GetComponent<SpriteRenderer>().material.DOFade(1,.6f);
+        max.GetComponent<SpriteRenderer>().material.DOFade(1,.6f);   
+        yield return new WaitForSeconds(.6f);
+        _state = GameState._start;
+        yield return null;
+    }
+    private void ResetParam()
+    {
         _spriteSize = sRenderer.bounds.size.y;
         _beerHight = - _spriteSize / 2;
         _foamHight = - _spriteSize / 2;
@@ -31,99 +120,30 @@ public class BottleController : MonoBehaviour
         Debug.Log(_spriteSize + " Max: " + max.position.y + " Min: " + min.position.y);
         liquidSpriteRenderer[0].material.SetFloat("_FillAmount1", _beerHight);
         liquidSpriteRenderer[1].material.SetFloat("_FillAmount1", _foamHight);
+        //status.SetText("");
+        StartCoroutine(SlideInTransition());
     }
-
-    private void FixedUpdate()
-    {
-        // if(!_isStop && _beerHight < 2.58f && _foamHight < 2.58f)
-        // {
-        //     _beerHight += 0.015f;
-        //     _foamHight += 0.025f;
-        //     liquidSpriteRenderer[0].material.SetFloat("_FillAmount1", _beerHight);
-        //     liquidSpriteRenderer[1].material.SetFloat("_FillAmount1", _foamHight);
-        // }
-        // else
-        // {
-        //     if(!_isEnd)
-        //     {
-        //         var c = _foamHight - _beerHight;
-        //         _temp = _beerHight + c / 2.5f;
-        //         _isEnd = true;
-        //     }
-        //     _foamHight -= 0.015f;
-        //     liquidSpriteRenderer[1].material.SetFloat("_FillAmount1", _foamHight);
-        //     if (!(_beerHight < _temp))
-        //     {
-        //         CheckResult();
-        //         return;
-        //     }
-        //     _beerHight += 0.01f;
-        //     liquidSpriteRenderer[0].material.SetFloat("_FillAmount1", _beerHight);
-        // }
-        Test();
-    }
-
-    public void OnClick()
-    {
-        //_isStop = true;
-    }
-    
     private void CheckResult()
     {
         //CHECK WIN CONDITION (Min <= a <= Max)
-        if(status.gameObject.activeInHierarchy) return;
+        //if(status.gameObject.activeInHierarchy) return;
         var minValue = min.localPosition.y;
         var maxValue = max.localPosition.y;
         if (_beerHight < maxValue && _beerHight > minValue)
         {
-            status.SetText("YOU WIN!");
+            //_state = GameState._end;
+            _score++;
+            status.SetText(_score.ToString());
+            StartCoroutine(SlideOutTransition());
             status.gameObject.SetActive(true);
-            Debug.Log("a = " + _beerHight + " maxValue = " + maxValue + " minValue = " + minValue);
+            Debug.Log("a = " + _beerHight + " maxValue = " + maxValue + " minValue = " + minValue + " state = " + _state.ToString());
         }
         else
         {
+            //_state = GameState._end;
             status.SetText("YOU LOSE!");
             status.gameObject.SetActive(true);
-            Debug.Log("a = " + _beerHight + " maxValue = " + maxValue + " minValue = " + minValue);
-        }
-    }
-
-    private void Test()
-    {
-        if (Input.touchCount > 0)
-        {
-            var touch = Input.GetTouch(0);
-            if (!_isEnd && touch.phase == TouchPhase.Stationary)
-            {
-                // GAMEPLAY
-                if(_beerHight < 2.58f && _foamHight < 2.58f)
-                {
-                    _beerHight += 0.015f;
-                    _foamHight += 0.025f;
-                    liquidSpriteRenderer[0].material.SetFloat("_FillAmount1", _beerHight);
-                    liquidSpriteRenderer[1].material.SetFloat("_FillAmount1", _foamHight);
-                }
-            }
-
-            if (!_isEnd && touch.phase is TouchPhase.Ended or TouchPhase.Canceled)
-            {
-                _isEnd = true;
-                var c = _foamHight - _beerHight;
-                _temp = _beerHight + c / 2.5f;
-            }
-            
-        }
-        if (_isEnd)
-        {
-            _foamHight -= 0.015f;
-            liquidSpriteRenderer[1].material.SetFloat("_FillAmount1", _foamHight);
-            if (!(_beerHight < _temp))
-            {
-                CheckResult();
-                return;
-            }
-            _beerHight += 0.01f;
-            liquidSpriteRenderer[0].material.SetFloat("_FillAmount1", _beerHight);
+            Debug.Log("a = " + _beerHight + " maxValue = " + maxValue + " minValue = " + minValue + " state = " + _state.ToString());
         }
     }
 }
